50b8b026522da5c1ef29aa7b415b0b1cee4c80bf
Andrei Skopenko <andrei.skopenko@paxful.com>
2019-02-05 22:19:29 +0200

    Add InSpec plugin

---
+++ README.md
@@ -0,0 +1,46 @@
+# InSpec plugin for ServerDensity
+
+## Overview
+
+[InSpec](https://www.inspec.io/) plugin for [ServerDensity](http://serverdensity.io).   
+Tested with sd-agent v2.2.1.
+
+## Installation
+
+* Install InSpec from `https://www.inspec.io/downloads/`
+* Copy the `inspec.py` script to `sd-agent` plugins folder `/usr/share/python/sd-agent/checks.d`
+* Copy configuration file `inspec.yaml.example` to `/etc/sd-agent/conf.d/inspec.yaml`.
+* Restart `sd-agent`
+
+## Metrics
+
+* `total` is the number of total inspec tests.
+
+* `failed` is the number of failed inspec tests.
+
+* `passed` is the number of passed inspec tests.
+
+## Contributing
+1. Fork the repository on Github
+2. Create a named feature branch (like `add_component_x`)
+3. Write your change
+4. Write tests for your change (if applicable)
+5. Run the tests, ensuring they all pass
+6. Submit a Pull Request using Github
+
+## License & Authors
+* Author:: Andrei Skopenko (@scopenco)
+
+```text
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ inspec.py
@@ -0,0 +1,75 @@
+#!/usr/share/python/sd-agent/bin/python
+#
+# Copyright (c) 2018 Andrei Skopenko <andrei@skopenko.net>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import sys
+import subprocess
+import os
+import yaml
+
+sys.path.append('/usr/share/python/sd-agent')
+from checks import AgentCheck
+
+
+class Beanstalkd(AgentCheck):
+    '''Tracks beanstalkd metrics
+    '''
+
+    def check(self, instance):
+
+        # check inspec binary
+        execute = '/usr/bin/inspec'
+        if not os.access(execute, os.X_OK):
+            raise Exception(u'InSpec binary not found')
+
+        # Attempt to load the profile_path setting from the instance config.
+        if 'profile_path' not in instance:
+            raise Exception(u'Please specify path to inspec profile')
+
+        # Attempt to load the tags from the instance config.
+        tags = instance.get('tags', [])
+
+        sp = subprocess.Popen((execute, 'exec', instance['profile_path'], '--attrs', instance['attributes_file'] if 'attributes_file' in instance else '', '--reporter', 'json-min'),
+                              stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = sp.communicate()
+        if err:
+            raise Exception(u'Could not execute inspec checks: %s' % err)
+
+        stats = yaml.load(out)
+        self.log.debug(u"InSpec raw data: {0}".format(stats))
+
+        metrics = {'passed': 0, 'failed': 0, 'total': 0}
+        for ctrl in stats['controls']:
+            metrics['total'] += 1
+            if ctrl['status'] == 'passed':
+                metrics['passed'] += 1
+            else:
+                metrics['failed'] += 1
+
+        for metric, value in metrics.iteritems():
+            try:
+                self.gauge('%s.%s' % ('inspec', metric), value, tags)
+            except Exception as e:
+                self.log.error(u'Could not submit metric: %s: %s' % (repr(metric), str(e)))
+
+        self.log.debug(u"InSpec metrics: {0}".format(metrics))
+
+if __name__ == '__main__':
+    # Load the check and instance configurations
+    check, instances = Beanstalkd.from_yaml('/etc/sd-agent/conf.d/inspec.yaml')
+    for instance in instances:
+        print "\nRunning the check against profile_path {} and attributes_file {}".format(instance['profile_path'], instance['attributes_file'])
+        check.check(instance)
+        print 'Metrics: {}'.format(check.get_metrics())
