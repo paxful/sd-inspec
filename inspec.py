562fc98bf4f8665aea3ce85ad075a5133138ecd8
Andrei Skopenko <andrei@skopenko.net>
2019-02-07 16:14:01 +0200

    Add black formatting

--- inspec.py
+++ inspec.py
@@ -19,65 +19,71 @@ import yaml
 import subprocess
 import os
 
-sys.path.append('/usr/share/python/sd-agent')
+sys.path.append("/usr/share/python/sd-agent")
 from checks import AgentCheck
 
 
 class Beanstalkd(AgentCheck):
-    '''Tracks beanstalkd metrics
-    '''
+    """Tracks beanstalkd metrics
+    """
 
     def check(self, instance):
 
         # check inspec binary
-        execute = '/usr/bin/inspec'
+        execute = "/usr/bin/inspec"
         if not os.access(execute, os.X_OK):
-            raise Exception(u'InSpec binary not found')
+            raise Exception(u"InSpec binary not found")
 
         # Attempt to load the profile_path setting from the instance config.
-        if 'profile_path' not in instance:
-            raise Exception(u'Please specify path to inspec profile')
+        if "profile_path" not in instance:
+            raise Exception(u"Please specify path to inspec profile")
 
         # Attempt to load the tags from the instance config.
-        tags = instance.get('tags', [])
+        tags = instance.get("tags", [])
 
-        cmd = [execute, 'exec', instance['profile_path']]
-        if 'attributes_file' in instance:
-            cmd.extend(['--attrs', instance['attributes_file']])
-        if 'controls' in instance:
-            cmd.append('--controls')
-            cmd.extend(instance['controls'])
-        cmd.extend(['--reporter', 'json-min'])
+        cmd = [execute, "exec", instance["profile_path"]]
+        if "attributes_file" in instance:
+            cmd.extend(["--attrs", instance["attributes_file"]])
+        if "controls" in instance:
+            cmd.append("--controls")
+            cmd.extend(instance["controls"])
+        cmd.extend(["--reporter", "json-min"])
 
-        sp = subprocess.Popen(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        sp = subprocess.Popen(
+            cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         out, err = sp.communicate()
         if err:
-            raise Exception(u'Could not execute inspec checks: %s' % err)
+            raise Exception(u"Could not execute inspec checks: %s" % err)
 
         stats = yaml.load(out)
         self.log.debug(u"InSpec raw data: {0}".format(stats))
 
-        metrics = {'passed': 0, 'failed': 0, 'total': 0}
-        for ctrl in stats['controls']:
-            metrics['total'] += 1
-            if ctrl['status'] == 'passed':
-                metrics['passed'] += 1
+        metrics = {"passed": 0, "failed": 0, "total": 0}
+        for ctrl in stats["controls"]:
+            metrics["total"] += 1
+            if ctrl["status"] == "passed":
+                metrics["passed"] += 1
             else:
-                metrics['failed'] += 1
+                metrics["failed"] += 1
 
         for metric, value in metrics.iteritems():
             try:
-                self.gauge('%s.%s' % ('inspec', metric), value, tags)
+                self.gauge("%s.%s" % ("inspec", metric), value, tags)
             except Exception as e:
-                self.log.error(u'Could not submit metric: %s: %s' % (repr(metric), str(e)))
+                self.log.error(
+                    u"Could not submit metric: %s: %s" % (repr(metric), str(e))
+                )
 
         self.log.debug(u"InSpec metrics: {0}".format(metrics))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Load the check and instance configurations
-    check, instances = Beanstalkd.from_yaml('/etc/sd-agent/conf.d/inspec.yaml')
+    check, instances = Beanstalkd.from_yaml("/etc/sd-agent/conf.d/inspec.yaml")
     for instance in instances:
         print "\nRunning the check against profile_path {} and attributes_file {}".format(
-            instance['profile_path'], instance['attributes_file'])
+            instance["profile_path"], instance["attributes_file"]
+        )
         check.check(instance)
-        print 'Metrics: {}'.format(check.get_metrics())
+        print "Metrics: {}".format(check.get_metrics())
